3d4ca33116fc00880ef1d6fbeb93b84ffbfadd0d
Daniel S <budartz@gmail.com>
2017-12-05 15:22:46 +0000

    finished the for and while loops
    finished the for and while loops exercises starting the functions

---
+++ Java_script_essentials_one.js
@@ -0,0 +1,95 @@
+
+// console.log('Hello world');
+// Exercises:
+
+// 1- Join the strings together with a space in between:
+
+// let string1 = "I",
+//     string2 = "like",
+//     string3 = "to",
+//     string4 = "boogie"
+//     string5 = "boogie";
+
+// let joined = string1 + " " + string2 + " " + string3 + " " + string4 + " " + string5;
+// console.log(joined); 
+
+// let joined = string1 + " " + string2 + " " + string3 + " " + string4 + " " + string5;
+// console.log(joined); 
+
+// // 2- Add up the numbers:
+// let number1 = 10,
+//     number2 = 20,
+//     dodgyNumber3 = "100",
+//     number4 = 40;
+
+// let sum = number1 + number2 + +dodgyNumber3 + number4;
+// console.log(sum); // 170
+
+// Conditionals
+
+// Use conditionals to complete the following:
+// 1-
+// let low = 10;
+// let high = 20;
+
+// if high is bigger than low log "high biggest"
+// if (high > low){
+
+// console.log("high biggest");
+// }
+
+// if low is bigger than high log "low biggest"
+
+// if (low > high){
+
+// console.log("low biggest");
+// }
+
+// try different values to make sure it works
+
+
+
+// 2-
+// let username = "daniel";
+// let password = "secret";
+
+// if username is admin and password is "secret" log "Admin"
+
+// if (username === "daniel" && password === "secret"){
+// 	console.log("Admin");
+// }
+// if username is admin but password is not "secret" log "Wrong Password"
+
+// if (username === "admin" && password !== "secret"){
+// 	console.log("Wrong Password");
+// }
+
+// if username is not admin log "Unknown User"
+
+// if (username !== "admin" && password === "secret"){
+// 	console.log("Wrong Password");
+// }
+
+// try different values to make sure it works
+
+// Loops
+
+// Write some code that lists the numbers from 1 to 100 using a for loop
+
+// 	for (let i=0; i<=100;i +=1){
+// console.log(i);
+// }
+
+// Write some code that lists the numbers from 100 to 1 using a for loop
+//  for (let i=100; i>=1; i-=1){
+// console.log(i);
+//  }
+
+// Write some code that lists all multiples of 13 up to 1,000 using a for loop
+ 
+// for (let i=1; i<=1000; i+=1) {
+// 	if (i%13 === 0){;
+
+//  console.log(i);
+//  }
+// }
---
+++ functions.js
@@ -0,0 +1,34 @@
+// 1 -Create a function, concat, that takes two strings as arguments. The function should return the two strings joined together with a space in between.
+
+
+// let concat = (a, b) =>{
+
+//   return (a + " " + b);
+// }
+// let result1 = concat("fish", "sticks");
+// let result2 =concat("hello", "world");
+// console.log(result2 + " " + result1);
+
+// 2 -Create a function, hello, that takes a string as an argument. The function should return the string "hello name".
+
+// let hello = (a)=>{
+
+// 	return ("hello" + " " + a);
+// }
+
+// console.log(hello("alice")); // "hello alice"
+// console.log(hello("bob")); // "hello bob"
+
+// 3 -Create a function, odd, that takes a number as an argument. The function should return true if the number is odd and false if it is even.
+
+// let odd = (n)=>{
+// 	if(n%2==0){
+// 		console.log(false);
+// 	}else
+// 	console.log(true);
+// }
+
+// let result1 = odd(1); // true
+// let result2 =odd(2); // false
+
+// console.log(result2 + " " + result1);
